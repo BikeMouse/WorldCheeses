1789b4836b12edf101f5cfda9be99683eba1e063
Judith Kraziewicz <judith.kraziewicz@gmail.com>
2023-04-10 14:23:34 +0100

    add final fixes

--- src/data.js
+++ src/data.js
@@ -6,6 +6,7 @@ export default [
         name: 'Cheddar',
         continent: 'Europe',
         description: "Cheddar cheese (or simply cheddar) is a natural cheese that is relatively hard, off-white (or orange if colourings such as annatto are added), and sometimes sharp-tasting. Cheddar originates from the English village of Cheddar in Somerset.Cheddar is the most popular cheese in the UK, accounting for 51% of the country's £1.9 billion annual cheese market.The UK produced 258,000 long tons (262,000 tonnes) in 2008.",
+        maps:'https://goo.gl/maps/YguaDK4cbdagEY9T8',
     },
     {
         id: 2,
@@ -14,6 +15,7 @@ export default [
         name: 'Monterey Jack',
         continent: 'North America',
         description: "Monterey Jack, sometimes shortened to Jack, is a US white, semi-hard cheese made using cow's milk, with a mild flavor and slight sweetness. It has been called 'an American original'.In addition to being eaten by itself, it is frequently marbled with Colby to produce Colby-Jack, or with yellow cheddar to produce cheddar-Jack. Pepper Jack is a version flavored with chili peppers and herbs. Dry Jack is a harder cheese with a longer aging time.",
+        maps:'https://goo.gl/maps/wX3kaaZ3Wv3veX4w9',
     },
     {
         id: 3,
@@ -22,14 +24,16 @@ export default [
         name: 'Queijo Minas',
         continent: 'South America',
         description: "Queijo Minas is a handmade cheese made from raw cow's milk, characterized by its tender, spongy texture and moistness. The flavor of Minas cheese is mild and slightly salty, depending on its ripeness and the region of production. The cheese is often used in the preparation of sandwiches, pancakes, and pastries.It is recommended to pair it with cold cuts, salads, and various vegetables that develop intense flavors after cooking.",
+        maps:'https://goo.gl/maps/91RsoxjevUcTBS2L7',
     },
     {
-        id: 5,
+        id: 4,
         coverImg: "Chihuahua.jpg",
         country: 'Mexico',
         name: 'Queso Chihuahua',
         continent: 'Central America',
         description: "In Mexico, queso Chihuahua is commonly recognized as a soft white cheese available in braids, balls or rounds and originates in the Mexican state of Chihuahua. In Chihuahua and in neighboring states, it is called queso menonita, after the Mennonite communities of Northern Mexico that first produced it, while elsewhere it is called queso Chihuahua.",
+        maps:'https://goo.gl/maps/X1yt3WpxBYc4m1VN6',
     },
     {
         id: 5,
@@ -38,6 +42,7 @@ export default [
         name: 'Halloumi',
         continent: 'Middle East',
         description: "Halloumi or Haloumi is a traditional Cypriot cheese made from a mixture of goat's and sheep's milk, and sometimes also cow's milk. Its texture is described as squeaky. It has a high melting point and so can easily be fried or grilled, a property that makes it a popular meat substitute. Halloumi was said to have originated from Bedouins around what is now Iraq, Syria, and Jordan and was imported to Cyprus. The methods of making halloumi likely originated sometime in the Medieval Byzantine period (AD 395–1191).",
+        maps:'https://goo.gl/maps/MMDmQRBhfzpeTZ92A',
     },
     {
         id: 6,
@@ -46,5 +51,6 @@ export default [
         name: 'Camembert',
         continent: 'Europe',
         description: "Camembert is a moist, soft, creamy, surface-ripened cow's milk cheese. It was first made in the late 18th century in Camembert, Normandy, in northwest France. It is sometimes compared in look and taste to brie cheese, albeit with a slightly lower butterfat content than brie's typical 60% and 75% by weight.In 1890, an engineer, M. Ridel, devised the wooden box that was used to carry the cheese and helped to send it for longer distances. These boxes are still used today.",
+        maps:'https://goo.gl/maps/L5WwGfgK3bZfpASaA',
     },
 ]
