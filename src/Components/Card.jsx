8d2b0b07dd973f26bfb1c7d0e28371d5dda876e2
Judith Kraziewicz <judith.kraziewicz@gmail.com>
2023-04-11 01:22:10 +0100

    add changes to fix links

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 export default function Card(props){
+    console.log(props);
     return (
         <div className='card'>
             <div className='flex'>
@@ -12,7 +13,7 @@ export default function Card(props){
                         <path fillRule="evenodd" clipRule="evenodd" d="M3.57866 5.25002C2.65532 5.25002 1.90837 4.50221 1.90837 3.57965C1.90837 2.65757 2.65532 1.90911 3.57866 1.90911C4.50167 1.90911 5.24927 2.65757 5.24927 3.57965C5.24927 4.50221 4.50167 5.25002 3.57866 5.25002ZM3.48394 0C1.54227 0 0 1.63678 0 3.65569C0 6.40791 2.95078 9.25191 2.95078 9.25191C3.34777 9.62783 3.59437 9.65847 4.01646 9.25191C4.01646 9.25191 7 6.40791 7 3.65569C7 1.63678 5.42544 0 3.48394 0Z" fill="#F55A5A" />
                     </svg>
                     <h2 className='country'>{props.country.toUpperCase()}</h2>
-                    <a href='${props.map}'>View on Google Maps</a>
+                    <a href={props.maps}>View on Google Maps</a>
                 </div>
                 <h1>{props.name}</h1>
                 <h3>{props.continent.toUpperCase()}</h3>
